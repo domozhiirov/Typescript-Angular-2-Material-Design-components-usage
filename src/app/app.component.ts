31329e33aca199a9c25eb267b680d4be83b7dcda
Vitaly Domozhiirov <domozhiirov@gmail.com>
2018-06-06 15:25:29 -0700

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,154 @@
-import { Component } from '@angular/core';
+import {FlatTreeControl} from '@angular/cdk/tree';
+import {Component, Injectable} from '@angular/core';
+import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
+import {BehaviorSubject, Observable, of as observableOf} from 'rxjs';
 
+/**
+ * File node data with nested structure.
+ * Each node has a filename, and a type or a list of children.
+ */
+export class FileNode {
+  children: FileNode[];
+  filename: string;
+  type: any;
+}
+
+/** Flat node with expandable and level information */
+export class FileFlatNode {
+  filename: string;
+  type: any;
+  level: number;
+  expandable: boolean;
+}
+
+/**
+ * The file structure tree data in string. The data could be parsed into a Json object
+ */
+const TREE_DATA = `
+{
+  "name1": {
+    "name": "Vitaly",
+    "phone": "4159927028",
+    "friends": {
+      "a1": {
+        "name": "Test2",
+        "phone": "6507712795"
+      },
+      "a2": {
+        "name": "Test3",
+        "phone": "6507712795",
+        "friends": {
+          "a1": {
+            "name": "Test2",
+            "phone": "6507712795"
+          }
+        }
+      }
+    }
+  },
+  "name2": {
+    "name": "Test1",
+    "phone": "6507712795"
+  }
+}
+`;
+
+/**
+ * File database, it can build a tree structured Json object from string.
+ * Each node in Json object represents a file or a directory. For a file, it has filename and type.
+ * For a directory, it has filename and children (a list of files or directories).
+ * The input will be a json object string, and the output is a list of `FileNode` with nested
+ * structure.
+ */
+@Injectable()
+export class FileDatabase {
+  dataChange: BehaviorSubject<FileNode[]> = new BehaviorSubject<FileNode[]>([]);
+
+  get data(): FileNode[] { return this.dataChange.value; }
+
+  constructor() {
+    this.initialize();
+  }
+
+  initialize() {
+    // Parse the string to json object.
+    const dataObject = JSON.parse(TREE_DATA);
+
+    // Build the tree nodes from Json object. The result is a list of `FileNode` with nested
+    //     file node as children.
+    const data = this.buildFileTree(dataObject, 0);
+
+    // Notify the change.
+    this.dataChange.next(data);
+  }
+
+  /**
+   * Build the file structure tree. The `value` is the Json object, or a sub-tree of a Json object.
+   * The return value is the list of `FileNode`.
+   */
+  buildFileTree(value: any, level: number): FileNode[] {
+    const data: any[] = [];
+    for (const k in value) {
+      const v = value[k];
+      const node = new FileNode();
+      node.filename = `${k}`;
+      if (v === null || v === undefined) {
+        // no action
+      } else if (typeof v === 'object') {
+        node.children = this.buildFileTree(v, level + 1);
+      } else {
+        node.type = v;
+      }
+      data.push(node);
+    }
+    return data;
+  }
+}
+
+/**
+ * @title Tree with flat nodes
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FileDatabase]
 })
 export class AppComponent {
-  title = 'app';
+
+  treeControl: FlatTreeControl<FileFlatNode>;
+
+  treeFlattener: MatTreeFlattener<FileNode, FileFlatNode>;
+
+  dataSource: MatTreeFlatDataSource<FileNode, FileFlatNode>;
+
+  constructor(database: FileDatabase) {
+    this.treeFlattener = new MatTreeFlattener(this.transformer, this._getLevel,
+      this._isExpandable, this._getChildren);
+    this.treeControl = new FlatTreeControl<FileFlatNode>(this._getLevel, this._isExpandable);
+    this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+    database.dataChange.subscribe(data => {
+      this.dataSource.data = data;
+    });
+  }
+
+  transformer = (node: FileNode, level: number) => {
+    const flatNode = new FileFlatNode();
+    flatNode.filename = node.filename;
+    flatNode.type = node.type;
+    flatNode.level = level;
+    flatNode.expandable = !!node.children;
+    return flatNode;
+  }
+
+  private _getLevel = (node: FileFlatNode) => node.level;
+
+  private _isExpandable = (node: FileFlatNode) => node.expandable;
+
+  private _getChildren = (node: FileNode): Observable<FileNode[]> => {
+    return observableOf(node.children);
+  }
+
+  hasChild = (_: number, _nodeData: FileFlatNode) => _nodeData.expandable;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MatButtonModule, MatCardModule, MatIconModule, MatTreeModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MatTreeModule, MatIconModule, MatCardModule, MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
